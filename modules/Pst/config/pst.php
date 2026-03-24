f7aedd8bbd3d26b429e947acd610fa16f47a2c22
jfsenechal <jfsenechal@gmail.com>
2026-03-24 19:00:14 +0100

    clean

--- modules/Pst/config/pst.php
+++ modules/Pst/config/pst.php
@@ -1,11 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     'validator' => [
         'email' => env('PST_VALIDATOR_EMAIL'),
-        'meili' => [
-            'index_name' => env('PST_MEILI_INDEX_NAME'),
-            'key' => env('MEILI_MASTER_KEY'),
-        ],
     ],
 ];
