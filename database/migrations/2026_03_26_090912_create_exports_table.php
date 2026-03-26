d04262bd339901f6d373b31dafca702ffda54a2a
Senechal jf <jfsenechal@gmail.com>
2026-03-26 16:42:10 +0100

    up

--- database/migrations/2026_03_26_090912_create_exports_table.php
+++ database/migrations/2026_03_26_090912_create_exports_table.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -23,7 +24,7 @@ return new class extends Migration
                 $table->unsignedInteger('processed_rows')->default(0);
                 $table->unsignedInteger('total_rows');
                 $table->unsignedInteger('successful_rows')->default(0);
-                $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+                $table->foreignIdFor(User::class)->cascadeOnDelete();
                 $table->timestamps();
             });
         }
