0b12a476e235e0436b72abbcfa9c8bd1e71b677a
Senechal jf <jfsenechal@gmail.com>
2026-03-26 14:24:09 +0100

    up

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -8,4 +8,10 @@ return [
     AcMarche\Security\Providers\SecurityPanelProvider::class,
     AcMarche\MailingList\Providers\Filament\MailingListPanelProvider::class,
     AcMarche\Pst\Providers\Filament\PstPanelProvider::class,
+    AcMarche\Courrier\Providers\CourrierPanelProvider::class,
+    AcMarche\Document\Providers\DocumentPanelProvider::class,
+    AcMarche\Hrm\Providers\HrmPanelProvider::class,
+    AcMarche\Mileage\Providers\MileagePanelProvider::class,
+    AcMarche\News\Providers\NewsPanelProvider::class,
+    AcMarche\Publication\Providers\PublicationPanelProvider::class,
 ];
