a63725f4b233be2a09343daa436dade936a11199
jfsenechal <jfsenechal@gmail.com>
2026-03-27 13:33:25 +0100

    remove user association from contacts

--- modules/MailingList/src/Repositories/OwnerScope.php
+++ modules/MailingList/src/Repositories/OwnerScope.php
@@ -1,12 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace AcMarche\MailingList\Repositories;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
 
-class OwnerScope implements Scope
+final class OwnerScope implements Scope
 {
     public function apply(Builder $builder, Model $model): void
     {
