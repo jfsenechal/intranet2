1c8f27c3f0b6b8bb506bf3dd523e42b022d32f38
Senechal jf <jfsenechal@gmail.com>
2026-04-02 11:42:27 +0200

    up

--- modules/Mileage/src/Observers/PersonalInformationObserver.php
+++ modules/Mileage/src/Observers/PersonalInformationObserver.php
@@ -4,4 +4,12 @@ declare(strict_types=1);
 
 namespace AcMarche\Mileage\Observers;
 
-final class PersonalInformationObserver {}
+use AcMarche\Mileage\Models\PersonalInformation;
+
+final class PersonalInformationObserver
+{
+    public function creating(PersonalInformation $personalInformation): void
+    {
+        $personalInformation->username = auth()->user()->username;
+    }
+}
