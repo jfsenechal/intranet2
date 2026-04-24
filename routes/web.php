2e3aad6101d20a93fe3e988ae99231c5794b9180
jfsenechal <jfsenechal@gmail.com>
2026-04-24 14:10:12 +0200

    up

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
+use App\Filament\Pages\Homepage;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/app', 301)
-    ->name('redirectHome');
+Route::get('/', fn () => redirect(Homepage::getUrl()))->name('homepage');
 
 Route::redirect('/login', '/app/login', 301)
     ->name('login');
